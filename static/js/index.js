407250ea359b1d67063c68724f8152bb80166425
nkubana0 <i.shema@alustudent.com>
2024-04-01 11:21:56 +0200

    media

--- static/js/index.js
+++ static/js/index.js
@@ -1,4 +1,4 @@
-const header = document.querySelector("header")
+/*const header = document.querySelector("header")
 
 window.addEventListener("scroll", function(){
     if(window.scrollY > 0){
@@ -7,4 +7,21 @@ window.addEventListener("scroll", function(){
     else {
         header.classList.remove("nav-active")
     }
-})
+})*/
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    menu.classList.toggle("open");
+}
+function closeMenu() {
+    const menu = document.querySelector(".menu-links");
+    menu.classList.remove("open");
+}
+document.addEventListener("click", function(event) {
+    const menu = document.querySelector(".menu-links");
+    const hamburgerIcon = document.querySelector(".hamburger-icon");
+
+    // Check if the click target is not the menu links or the hamburger icon
+    if (!menu.contains(event.target) && !hamburgerIcon.contains(event.target)) {
+        closeMenu(); // Close the menu
+    }
+});
